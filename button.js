faae298c1a6ba4490ec13888b55462f80b77d10a
stainlesteel <jasminedeol67@gmail.com>
2025-07-04 15:48:17 -0600

    base files

---
+++ button.js
@@ -0,0 +1,32 @@
+const discord = document.getElementById('discord');
+        discord.addEventListener('click', function() {
+            window.open('https://discordapp.com/users/1105250391320449044', '_blank');
+        })
+const email = document.getElementById('email');
+        email.addEventListener('click', function() {
+            window.open('mailto:pd4500@protonmail.com', '_self');
+        })
+const yt = document.getElementById('youtube');
+        yt.addEventListener('click', function() {
+            window.open('https://www.youtube.com/@stainlesssteel275', '_blank');
+        })
+const github = document.getElementById('github');
+        github.addEventListener('click', function() {
+            window.open('https://github.com/stainlesteel', '_blank');
+        })
+const lastfm = document.getElementById('lastfm');
+        lastfm.addEventListener('click', function() {
+            window.open('https://www.last.fm/user/pd684_', '_blank');
+        })
+const steam = document.getElementById('steam');
+        steam.addEventListener('click', function() {
+            window.open('https://steamcommunity.com/profiles/76561199475166512', '_blank');
+        })
+const reddit = document.getElementById('reddit');
+        reddit.addEventListener('click', function() {
+            window.open('https://www.reddit.com/user/stainlessteel601/', '_blank');
+        })
+const tiktok = document.getElementById('tiktok');
+        tiktok.addEventListener('click', function() {
+            window.open('https://www.tiktok.com/@stainless.steel904', '_blank');
+        })                
---
+++ project.js
@@ -0,0 +1,12 @@
+const ubs64 = document.getElementById('ubs');
+        ubs64.addEventListener('click', function() {
+            window.open('https://www.ubs64.xyz', '_blank');
+        });  
+const qrcode = document.getElementById('qrcode');
+        qrcode.addEventListener('click', function() {
+            window.open('https://github.com/stainlesteel/CLI_QRCode', '_blank');
+        });  
+const aibot = document.getElementById('aibot');
+        aibot.addEventListener('click', function() {
+            window.open('https://github.com/stainlesteel/ai-discord-bot', '_blank');
+        });  
